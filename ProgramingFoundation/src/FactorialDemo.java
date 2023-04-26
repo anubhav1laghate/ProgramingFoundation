7779b8960087c0e6594a6ff7019960b527206b87
Administrator <Administrator@172.31.31.34>
2023-04-26 07:40:55 +0000

    Push to commit

---
+++ ProgramingFoundation/src/FactorialDemo.java
@@ -0,0 +1,37 @@
+
+public class factorialDemo {
+
+public static void main(String[] args) {
+
+        //5! = 5*4*3*2*1
+
+        //
+
+        int number = 5;
+
+
+        int result = 1;
+
+        int counter =number;
+
+
+        while(counter>=1)
+
+        {    result = result * counter;
+
+            counter = counter - 1;
+
+        }
+
+
+ 
+
+System.out.println("Fact reult"+ result);
+
+}
+
+}
+
+ 
+
+
---
+++ ProgramingFoundation/src/PerfectNumber.java
@@ -0,0 +1,29 @@
+
+public class PerfectNumber {
+
+
+	public static boolean isPerfectNumber(int number) {
+	
+		int half = number / 2;
+		int sum = 0;
+		
+		int counter  = 1;
+		while(counter<=half)}
+	
+	remainder = number % counter;
+	if(remainder ==0) {
+		sum = sum + counter;
+	}
+		counter++;// counter = counter +1
+		
+}
+
+system.out.println(sum);
+return false;
+
+}
+
+}
+
+		
+		
